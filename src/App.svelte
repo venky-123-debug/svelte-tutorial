8389c92f3d03b495d0bd763ea6dd47305f7aaf97
venky-123-debug <venky26494@yahoo.com>
2023-02-15 23:54:57 +0530

    images added to public folder

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,7 @@
 <script>
-  export let name
 </script>
 
-<main>
-  <h1>Hello {name}!</h1>
-  <p>
-    Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a>
-    to learn how to build Svelte apps.
-  </p>
-</main>
+<main />
 
 <style global lang="postcss">
   @tailwind base;
