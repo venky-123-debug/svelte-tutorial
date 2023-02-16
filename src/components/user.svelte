0af47658df865b294413744beb0066411c938576
venky-123-debug <venky26494@yahoo.com>
2023-02-16 10:34:33 +0530

    user array has been loaded from user component

--- src/components/user.svelte
+++ src/components/user.svelte
@@ -1,16 +1,38 @@
 <script>
-  let user = {
-    userName: "John",
-    userImage: "assets/images/user3.png",
-    emailId: "john@email.com",
-  }
+  let user = [
+    {
+      userName: "John",
+      userImage: "assets/images/user3.png",
+      emailId: "john@email.com",
+    },
+    {
+      userName: "John",
+      userImage: "assets/images/user2.png",
+      emailId: "john@email.com",
+    },
+    {
+      userName: "John",
+      userImage: "assets/images/user1-1.png",
+      emailId: "john@email.com",
+    },
+  ]
 </script>
 
 <h1>List Of Users</h1>
 <div>
-  <img src={user.userImage} class="h-24 w-24 rounded-md" alt="" />
-  <p>{user.emailId}</p>
-  <p>{user.userName}</p>
+  <img src={user[0].userImage} alt={user[0].userName} />
+  <p>{user[0].userName}</p>
+  <p>{user[0].emailId}</p>
+</div>
+<div>
+  <img src={user[1].userImage} alt={user[1].userName} />
+  <p>{user[1].userName}</p>
+  <p>{user[1].emailId}</p>
+</div>
+<div>
+  <img src={user[0].userImage} alt={user[0].userName} />
+  <p>{user[0].userName}</p>
+  <p>{user[0].emailId}</p>
 </div>
 
 <style>
