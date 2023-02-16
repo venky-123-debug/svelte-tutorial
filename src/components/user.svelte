b5c2ffefac9665553f032dddedbeb3f46e14f301
venky-123-debug <venky26494@yahoo.com>
2023-02-16 10:48:59 +0530

    each loop completed on user component

--- src/components/user.svelte
+++ src/components/user.svelte
@@ -1,39 +1,36 @@
 <script>
-  let user = [
+  let users = [
+    { id: 1, name: "John", image: "assets/images/user3.png", mail: "john@email.com" },
     {
-      userName: "John",
-      userImage: "assets/images/user3.png",
-      emailId: "john@email.com",
+      id: 2,
+      name: "John",
+      image: "assets/images/user2.png",
+      mail: "john@email.com",
     },
     {
-      userName: "John",
-      userImage: "assets/images/user2.png",
-      emailId: "john@email.com",
-    },
-    {
-      userName: "John",
-      userImage: "assets/images/user1-1.png",
-      emailId: "john@email.com",
+      id: 3,
+      name: "John",
+      image: "assets/images/user1-1.png",
+      mail: "john@email.com",
     },
   ]
+
+  const getUser = () => {
+    return users
+  }
 </script>
 
 <h1>List Of Users</h1>
-<div>
-  <img src={user[0].userImage} alt={user[0].userName} />
-  <p>{user[0].userName}</p>
-  <p>{user[0].emailId}</p>
-</div>
-<div>
-  <img src={user[1].userImage} alt={user[1].userName} />
-  <p>{user[1].userName}</p>
-  <p>{user[1].emailId}</p>
-</div>
-<div>
-  <img src={user[0].userImage} alt={user[0].userName} />
-  <p>{user[0].userName}</p>
-  <p>{user[0].emailId}</p>
-</div>
+{#each getUser() as { image, name, mail, id }, i (id)}
+  <div>
+    <p>{i + 1}</p>
+    <img src={image} alt={name} />
+    <p>{name}</p>
+    <p>{mail}</p>
+  </div>
+{:else}
+  <p>No users found</p>
+{/each}
 
 <style>
 </style>
